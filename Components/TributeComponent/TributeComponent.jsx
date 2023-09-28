36cb95cf4dbd4102a70d0e85eb12e40766d4d6b9
akashmm <akashmm92@gmail.com>
2023-09-28 11:02:52 +0530

    getting started with portfolio

---
+++ Components/TributeComponent/TributeComponent.jsx
@@ -0,0 +1,14 @@
+import virgil from '../../src/assets/virgil.png'
+import './TributeComponent.css'
+
+
+const TributeComponent = ()=>{
+    return(
+        <>
+            {/* <h2>ING."*QUESTION EVERYTHING TO ME MEANS TO ALWAYS RECONSIDER CONCEPTS. I INTERPRET THIS FLAG AS A VOW TO DECONSTRUCT EVERY CONCEPT POSED IN OUR WAY AND TRY TO FIND IT’S INEFFICIENCIES. QUESTION EVERYTHING MEANS NOT TO [PRE]ACCEPT A CONCEPT WITHOUT QUESTIONING IT FIRST.*</h2> */}
+            {/* <img src={virgil} style={{ width: '50%', height: 'auto' }}></img> */}
+        </>
+    )
+}
+
+export default TributeComponent;
