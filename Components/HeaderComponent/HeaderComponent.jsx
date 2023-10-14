4fac5fe661ad50f4545c70e8e1106eb695f05cf2
akashmm <akashmm92@gmail.com>
2023-10-14 18:04:44 +0530

    added crossroads

--- Components/HeaderComponent/HeaderComponent.jsx
+++ Components/HeaderComponent/HeaderComponent.jsx
@@ -4,7 +4,7 @@ const HeaderComponent = ()=>{
     return(
         <>
             <div className='banner'>
-                <h1></h1>
+                <h1>" HEADER "</h1>
             </div>
             <div className="portfolio">
                 <h1>" PORTFOLIO "</h1>
