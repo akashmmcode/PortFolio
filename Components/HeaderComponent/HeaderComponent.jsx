a005ab05ed4911fb8955b598f1dbd2621501c4c7
Akash <akashmm92@gmail.com>
2023-10-16 20:11:20 +0530

    new feature

--- Components/HeaderComponent/HeaderComponent.jsx
+++ Components/HeaderComponent/HeaderComponent.jsx
@@ -1,16 +1,28 @@
-import './HeaderComponent.css'
+import "./HeaderComponent.css";
 
-const HeaderComponent = ()=>{
-    return(
-        <>
-            <div className='banner'>
-                <h1>" HEADER "</h1>
-            </div>
-            <div className="portfolio">
-                <h1>" PORTFOLIO "</h1>
-            </div>
-        </>
-    )
-}
+const HeaderComponent = () => {
+  return (
+    <>
+      <div className="banner">
+        <div>
+          <h4>" HEADER "</h4>
+        </div>
+        <section className="color-pallet">
+          <div style={{ backgroundColor: "yellow" }}></div>
+          <div style={{ backgroundColor: "pink" }}></div>
+          <div style={{ backgroundColor: "blue" }}></div>
+          <div style={{ backgroundColor: "purple" }}></div>
+          <div style={{ backgroundColor: "green" }}></div>
+          <div style={{ backgroundColor: "orange" }}></div>
+          <div style={{ backgroundColor: "lightpink" }}></div>
+          <div style={{ backgroundColor: "lightblue" }}></div>
+        </section>
+      </div>
+      <div className="portfolio">
+        <h1>" PORTFOLIO "</h1>
+      </div>
+    </>
+  );
+};
 
 export default HeaderComponent;
