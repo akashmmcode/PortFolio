ddd95dbe96abd1accdc7ba26165b12043fff7572
akashmm <akashmm92@gmail.com>
2023-12-13 18:36:48 +0530

    new changes

--- Components/CrossLineComponent/CrossLine.jsx
+++ Components/CrossLineComponent/CrossLine.jsx
@@ -6,7 +6,8 @@ const CrossLine = () => {
       <div className="portfolio">
         <h1></h1>
       </div>
-      <h1>"PORTFOLIO"</h1>
+      <marquee><h1>HELLO I'm AKASH</h1></marquee>
+      <h1>"THIS IS A PORTFOLIO"</h1>
     </>
   );
 };
