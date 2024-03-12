ea0d48d66f51dbe3d78b6c698d2596cadbcc516f
akashmm <akashmm92@gmail.com>
2024-03-12 19:03:45 +0530

    new chnages

--- Components/CrossLineComponent/CrossLine.jsx
+++ Components/CrossLineComponent/CrossLine.jsx
@@ -10,8 +10,12 @@ const CrossLine = () => {
       <div className="portfolio">
         <h1></h1>
       </div>
-      <div className="marquee-container">
+      {/* <div className="marquee-container">
         <h1 className="marquee-text">"WEB DEVELOPER" - "DIGITAL DESIGNER"</h1>
+      </div> */}
+
+      <div className="name">
+        <h1>Portfolio</h1>
       </div>
       <h1 className="port">"THIS IS A PORTFOLIO"</h1>
     </>
