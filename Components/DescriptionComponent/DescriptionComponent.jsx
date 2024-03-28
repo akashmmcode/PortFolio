7369acc63de1cc667fec21af3894f82e8124597c
akashmm <akashmm92@gmail.com>
2024-03-28 18:33:50 +0530

    new addons

--- Components/DescriptionComponent/DescriptionComponent.jsx
+++ Components/DescriptionComponent/DescriptionComponent.jsx
@@ -23,50 +23,38 @@ const DescriptionComponent = () => {
     <>
       <div className="about_outer_div">
         <div className="front_end_addict_outer_div">
-          <h1 className="front_end">FRONT-END</h1>
-          <div className="horizontal-line"></div>
 
-          <h1 className="addict">ADDICT!</h1>
+
+          <h1 className="front_end">FR<span className="short">O</span>NT-END</h1>
+          <div className="horizontal-line"></div>
+          <h1 className="addict">ADDI<span className="shortc">C</span>T!</h1>
           <div className="horizontal-line"></div>
 
           <div>
             <img className="techstack" src={photo2}></img>
           </div>
           <div className="description_container">
-          <h1 className="description">
-            As a multidisciplinary freelancer, I'm passionate about creating
-            iconic digital experiences through motion, typography, and creative
-            coding for companies and agencies around the world.
-          </h1>
+            <h1 className="description">
+              As multidisciplinary freelancer, I'm passionate about creating
+              iconic digital experiences through motion, typography, and
+              creative coding for companies and agencies around the world.
+            </h1>
           </div>
         </div>
         <div className="photo_outer_div">
           <img className="profilephoto" src={photo1}></img>
-          <h1>DESIGN-VELOPER</h1>
-          <h1>CREATIVE DEVELOPER</h1>
-          <h1>BASED IN BANGALORE,IN.</h1>
+          <h1>DESI<span className="short">G</span>N-VEL<span className="short">O</span>PER</h1>
+          <div className="horizontal-line"></div>
+          <h1><span className="short">C</span>REATIVE DEVEL<span className="short">O</span>PER</h1>
+          <div className="horizontal-line"></div>
+          <h1>BASED IN BAN<span className="short">G</span>AL<span className="short">O</span>RE,IN.</h1>
+          <div className="horizontal-line"></div> 
+
         </div>
       </div>
 
       <div className="desc">
-        <section>
-          <h3>
-            Driven by a commitment to break down the conventional barriers,
-            transforming into a true "DESIGN-VELOPER" -someone who crafts user
-            experiences with the precision of a designer and the technical
-            finesse of a developer.
-          </h3>
-          <img className="barcode" src={akash}></img>
-        </section>
-        <div className="blackbanner">
-          <h1>"</h1>
-          <h1>"</h1>
-        </div>
-        <div className="details">
-          <h5>AKASH.MM - BANGLORE</h5>
-          <h5>FRONT END DEVELOPER</h5>
-          <h5>JULY 12,2021 - PRESENT</h5>
-        </div>
+       <h1>WEBSITE</h1>
       </div>
     </>
   );
